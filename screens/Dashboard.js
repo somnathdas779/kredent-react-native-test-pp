f5418df7daabcb9a7feee548c2ec15101f29b58d
somnathdas779 <somnathdas779@gmail.com>
2020-11-23 02:42:20 +0530

    Add files via upload

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -19,22 +19,6 @@ export default class Dashboard extends Component {
     super(props);
     this.state = {
       data: [
-        {id:17, title: "Entry Pass",      color:"#FF69B4", members:8,  image:"https://img.icons8.com/color/70/000000/name.png"},
-        {id:1, title: "Add Members",     color:"#87CEEB", members:6,  image:"https://img.icons8.com/office/70/000000/home-page.png"},
-        {id:2, title: "Entry Logs",     color:"#4682B4", members:12, image:"https://img.icons8.com/color/70/000000/two-hearts.png"} ,
-        {id:3, title: "My Family",   color:"#6A5ACD", members:5,  image:"https://img.icons8.com/color/70/000000/family.png"} ,
-        {id:4, title: "My Relative",  color:"#FF69B4", members:6,  image:"https://img.icons8.com/color/70/000000/groups.png"} ,
-        {id:5, title: "My Maid",   color:"#00BFFF", members:7,  image:"https://img.icons8.com/color/70/000000/classroom.png"} ,
-        {id:6, title: "Parking",   color:"#00FFFF", members:8,  image:"https://img.icons8.com/dusk/70/000000/checklist.png"} ,
-        {id:8, title: "Forum",    color:"#20B2AA", members:23, image:"https://img.icons8.com/dusk/70/000000/globe-earth.png"} ,
-        {id:9, title: "Complain", color:"#191970", members:45, image:"https://img.icons8.com/color/70/000000/to-do.png"} ,
-        {id:10, title: "Access Facility",     color:"#87CEEB", members:13, image:"https://img.icons8.com/color/70/000000/basketball.png"} ,
-        {id:11, title: "My Enpences",     color:"#4682B4", members:13, image:"https://img.icons8.com/color/70/000000/basketball.png"} ,
-        {id:12, title: "Payment",     color:"#6A5ACD", members:13, image:"https://img.icons8.com/color/70/000000/basketball.png"} ,
-        {id:13, title: "settings",     color:"#20B2AA", members:13, image:"https://img.icons8.com/color/70/000000/basketball.png"} ,
-        {id:14, title: "Contact Owner",     color:"#008080", members:13, image:"https://img.icons8.com/color/70/000000/basketball.png"} ,
-        {id:15, title: "Contact Security",     color:"#4682B4", members:13, image:"https://img.icons8.com/color/70/000000/basketball.png"} ,
-        {id:15, title: "Contact Security",     color:"#FF69B4", members:13, image:"https://img.icons8.com/color/70/000000/basketball.png"} 
       ]
     };
   }
@@ -57,15 +41,7 @@ export default class Dashboard extends Component {
           renderItem={({item}) => {
             return (
               <TouchableOpacity style={[styles.card, {backgroundColor:item.color}]} onPress={() => {this.clickEventListener(item.view)}}>
-                <View style={styles.cardHeader}>
-                  <Text style={styles.title}>{item.title}</Text>
-                 
-                </View>
-               
-                <AntDesign style={styles.cardImage} name="android1" size={60} color="#fff" />
-                <View style={styles.cardFooter}>
-                  <Text style={styles.subTitle}>{item.members} members</Text>
-                </View>
+                
               </TouchableOpacity>
             )
           }}/>
